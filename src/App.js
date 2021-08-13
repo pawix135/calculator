a8a8c7fff8e8458dc4fd24427933c52fac64c136
pawix135 <leagueoflegendsoppl@gmail.com>
2021-08-13 22:35:17 +0200

    end :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import TopSection from './components/ThemeSection';
+import ResultsSection from './components/ResultsSection'
+import KeysSection from './components/KeysSection'
+import { useState } from 'react';
+import { light, dark, hc } from './Themes/themes'
+import { GlobalStyles } from "./Themes/globalStyles"
 
-function App() {
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  width: 500px;
+  @media (max-width: 500px){
+    width: 80vw;
+
+  }
+
+`
+
+const App = () => {
+
+  const [theme, setTheme] = useState('dark');
+  const [math, setMath] = useState("");
+
+  const changeTheme = (type) => {
+    setTheme(type)
+  }
+
+  const thm = () => {
+    switch( theme ){
+      case "dark":
+        return dark;
+      case "light":
+        return light;
+      case "hc":
+        return hc;
+      default:
+        return dark;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={thm} >
+      <GlobalStyles />
+      <Wrapper>
+        <TopSection changeTheme={changeTheme} />
+        <ResultsSection math={math} />
+        <KeysSection setMath={setMath} math={math} />
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
